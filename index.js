cfdbab48d8c2d4934e4e71d91f93e9e997073b38
MiikaJok <miika.jokinen10@hotmail.com>
2023-03-13 15:08:08 +0200

    kissoja

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 'use strict';
-const _ = require('lodash');
 
-const output = _.camelCase('Foo Bar')
-console.log(output);
-console.log("MOROR");
-console.log("heiheimitäkuuluu");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+app.get("/catinfo", (req, res) => {
+    const cat = {
+        name: "Frank",
+        birthdate: "2010-12-25",
+        weight: 5,
+    };
+    res.json(cat);
+});
