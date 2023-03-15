8e36a257a61566396032e1c48d9dd1a33ff13840
MiikaJok <miika.jokinen10@hotmail.com>
2023-03-15 13:21:23 +0200

    server-two toimii

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+    const name = "name";
+    const birthdate = "birthdate";
+    const weight = "weight";
+    const content = {name, birthdate, weight};
+    res.render('index', content);
+    });
 
 app.get("/catinfo", (req, res) => {
     const cat = {name: "Frank", birthdate: "2010-12-25", weight: 5
     };
     res.json(cat);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
