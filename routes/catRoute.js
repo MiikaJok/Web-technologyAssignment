e09410ac7268c9278c5ff92afe7b5451716b54a2
MiikaJok <miika.jokinen10@hotmail.com>
2023-03-20 11:29:00 +0200

    controllereja ja modeleita

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,20 @@
 'use strict';
-// catRoute
+const express = require('express');
+const router = express.Router();
+const catController = require('../controllers/catController');
+module.exports = router;
+
+router.get('/', catController.getCatList);
+router.get('/:catId', catController.getCat);
+
+router.post('/', (req, res) => {
+    res.send('From this endpoint you can add cats.');
+});
+
+router.put('/', (req, res) => {
+    res.send('From this endpoint you can modify a cat.');
+});
+
+router.delete('/', (req, res) => {
+    res.send('From this endpoint you can delete cats.');
+});
