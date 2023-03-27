42bc787399fd8f94c385ec9a05493cdb2be75d29
MiikaJok <miika.jokinen10@hotmail.com>
2023-03-27 20:21:50 +0300

    userhommat loppuun

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userConstroller');
-module.exports = router;
 
-router.get('/', UserController.getUserList);
-router.get('/:userId', UserController.getUser);
-router.post('/', UserController.postUser);
+router.route('/')
+    .get(UserController.getUserList)
+    .post(UserController.postUser)
+router.route('/:id')
+    .get(UserController.getUser)
 //router.put('/', UserController.modifyUser);
 //router.delete('/', UserController.deleteUser);
 
 //TODO: add another endpoints needed
 //TODO: add validation &
+module.exports = router;
