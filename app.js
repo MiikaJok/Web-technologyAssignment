d696a01bd66c2bb2ff1e477a80179725c603e307
MiikaJok <miika.jokinen10@hotmail.com>
2023-04-03 11:32:43 +0300

    session hommaa

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
 'use strict';
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 const port = 3000;
+
+const user = {name: "foo", password: "bar"};
 
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(session({
+  secret: "maanantai",
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.get('/', (req, res) => {
 
+app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.get('/secret', (req, res) => {
+  if (req.session.loggedIn) {
+    res.render("secret");
+  } else {
+    res.status(404).send("You must login to see this secret!");
+  }
+});
+
+app.post("/login", (req,res) => {
+  console.log("trying to log in", req.body);
+  if (req.body.username === user.name
+      && req.body.password === user.password) {
+    req.session.loggedIn = true;
+    res.redirect("/secret");
+  } else {
+    res.status(401).send("login failed!");
+  }
+});
+app.get("/logout", (req,res) => {
+  req.session.loggedIn = false;
+  res.redirect("form");
 });
 
 app.get("/setCookie/:color", (req,res) => {
